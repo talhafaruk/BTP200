c405f8e2d5ad90ce81b6fb448fb049522b5dbd70
talhafaruk <talhaf4@hotmail.com>
2016-02-08 18:43:30 -0500

    Create Enrollment.cpp

---
+++ w4/in_lab/Enrollment.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstring>
+#include "Enrollment.h"
+using namespace std;
+
+namespace sict
+{
+
+    // missing constructors and member functions
+    
+    bool Enrollment::isValid() const
+    {
+        return name[0];
+    }
+    
+    void Enrollment::setEmpty()
+    {
+        name[0] = '\0';
+        code[0] = '\0';
+        year = 0;
+        semester = 0;
+        timeSlot = 0;
+        enrolled = false;
+    }
+    
+    bool Enrollment::isEnrolled() const
+    {
+        return enrolled;
+    }
+    
+    void Enrollment::display(bool nameOnly) const
+    {
+        if (isValid())
+        {
+            cout << name;
+            if(!nameOnly)
+            {
+                cout << endl << code << ", Year: " << year << " semester: " << semester << " Slot: " << timeSlot << endl;
+                cout << "Status: " << (isEnrolled() ? "E" : "Not e") << "nrolled." << endl;
+            }
+        }
+        else
+        {
+            cout << "Invalid enrollment!" << endl;
+        }
+    }
+}
+
+
+
